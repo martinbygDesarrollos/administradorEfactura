0819f1abce0d68244a26b9944a78ff28f93f9d26
Martin B&G Desarrollo de Sistemas <martin@hit.com.uy>
2022-08-10 15:41:09 -0300

    vista de los vencimientos de certificados y caes

--- src/controllers/ctr_companies.php
+++ src/controllers/ctr_companies.php
@@ -1,6 +1,6 @@
 <?php
 
-//require_once '../src/class/users.php';
+require_once '../src/class/utils.php';
 require_once 'ctr_rest.php';
 
 
@@ -8,11 +8,59 @@ class ctr_companies{
 
 	public function getCompanies(){
 		$restController = new ctr_rest();
+		$utilClass = new utils();
 		$responseCompanies = $restController->getCompanies();
-		foreach ($responseCompanies->listResult as $key => $value) {
-			if (strlen($value->certificateExpireDate)>0){
-				$responseCompanies->listResult[$key]->vencimientos = substr($value->certificateExpireDate,8,2)."/".substr($value->certificateExpireDate,5,2)."/".substr($value->certificateExpireDate,0,4);
-			}else $responseCompanies->listResult[$key]->vencimientos = "";
+
+
+		foreach ($responseCompanies->listResult as $key => $value) { //recorriendo empresas
+			$expireDate = null;
+			$expireDateVoucher = null;
+			$expireDateCaeType = null;
+
+			if ( isset($value->certificateExpireDate) && $value->certificateExpireDate != "" ){
+				$certExpireDate = substr($value->certificateExpireDate,0,4).substr($value->certificateExpireDate,5,2).substr($value->certificateExpireDate,8,2);
+
+				$expireDate = $certExpireDate;
+				$expireDateVoucher = "Certificado";
+				$expireDateCaeType = 0;
+			}
+
+			foreach ($value->caes as $cae) {
+
+				if ( isset($expireDate) && $expireDate != ""  ){
+					$expireDateCae = $cae->vencimiento;
+					$date = substr($expireDateCae,0,4).substr($expireDateCae,5,2).substr($expireDateCae,8,2);
+
+					if ( $date < $expireDate ){
+						$duplicatedCae = false;
+						foreach ($value->caes as $cae2) {
+							if ( $cae->tipoCFE == $cae2->tipoCFE && $cae2->vencimiento > $cae->vencimiento ){
+								$duplicatedCae = true;
+							}
+						}
+
+						if ( !$duplicatedCae ){
+							$expireDate = $date;
+							$expireDateVoucher = "CAE";
+							$expireDateCaeType = $utilClass->tableCfeType($cae->tipoCFE);
+						}
+
+					}
+
+				}else {
+					$expireDateCae = $cae->vencimiento;
+					$date = substr($expireDateCae,0,4).substr($expireDateCae,5,2).substr($expireDateCae,8,2);
+
+					$expireDate = $date;
+					$expireDateVoucher = "CAE";
+					$expireDateCaeType = $utilClass->tableCfeType($cae->tipoCFE);
+				}
+			}
+
+			$responseCompanies->listResult[$key]->vencimientos = substr($expireDate,6,2)."/".substr($expireDate,4,2)."/".substr($expireDate,0,4);
+			$responseCompanies->listResult[$key]->comprobante = $expireDateVoucher;
+			$responseCompanies->listResult[$key]->tipoCae = $expireDateCaeType;
+
 		}
 
 		return $responseCompanies;
