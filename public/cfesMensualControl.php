b3a24b2ad25c3808094e1e36805423adee3aba21
Martin B&G Desarrollo de Sistemas <martin@hit.com.uy>
2022-10-05 17:13:54 -0300

    contar cantidad de comprobantes por tipo cfe

--- public/cfesMensualControl.php
+++ public/cfesMensualControl.php
@@ -9,7 +9,7 @@ $connection = new mysqli(DB_HOST, DB_USR, DB_PASS, DB_DB) or die("No se puede co
 $connection->set_charset("utf8");
 if($connection){
 	echo nl2br("Conexión base de datos\n");
-	$query = $connection->prepare("SELECT tokenRest FROM `usuarios` WHERE correo = 'guillermo@gargano.com.uy'");
+	$query = $connection->prepare("SELECT tokenRest FROM `usuarios` WHERE correo = 'desarrollo@byg.uy'");
 	$query->execute();
 	$result = $query->get_result();
 	$tokenRest = $result->fetch_object()->tokenRest;
@@ -31,123 +31,135 @@ $resultado = file_get_contents(URL_REST.'companies', false, $contexto);//rest co
 $resultCompanies = json_decode($resultado);
 echo nl2br("Se obtienen los datos de ".count($resultCompanies)." empresas\n");
 foreach( $resultCompanies as $value ){
-	array_push($arrayCompanies, $value->rut);
+	array_push($arrayCompanies, array("rut"=>$value->rut, "estado"=>$value->estado));
 }
 
+$from = date('Ymd', strtotime("- 1 month" , strtotime(date("Y-m")."-01")));
+$to = date('Ymt', strtotime("- 1 month" , strtotime(date("Y-m-d"))));
 
-$from = "20220901000000";
-$to = "20220930235959";
-$limit = "2147483647";
+echo nl2br("Período de tiempo a consultar ".$from." - ".$to."\n");
 
-foreach ($arrayCompanies as $rut) {
-	echo nl2br("Empresa ".$rut."\n");
+foreach ($arrayCompanies as $companie) {
 
-	$ticket = 0;//case 101: return "e-Ticket";
-	$ncticket = 0; //case 102: return "Nota de crédito de e-Ticket";
-	$ndticket = 0; //case 103: return "Nota de débito de e-Ticket";
-	$ticketconti = 0;//case 201: return "e-Ticket Contingencia";
-	$factura = 0; //case 111: return "e-Factura";
-	$ncfactura = 0; //case 112: return "Nota de crédito de e-Factura";
-	$ndfactura = 0; //case 113: return "Nota de débito de e-Factura";
-	$facturaconti = 0; //case 211: return "e-Factura Contingencia";
-	$facturaexp = 0; //case 121: return "e-Factura Exportación";
-	$ncfacturaexp = 0; //case 122: return "Nota de crédito de e-Factura Exportación";
-	$ndfacturaexp = 0; //case 123: return "Nota de débito de e-Factura Exportación";
-	$remitoexp = 0; //case 124: return "e-Remito de Exportación";
-	$ticketctaaje = 0; //case 131: return "e-Ticket Venta por Cuenta Ajena";
-	$ncticketctaaje = 0; //case 132: return "Nota de crédito de e-Ticket Venta por Cuenta Ajena";
-	$ndticketctaaje = 0; //case 133: return "Nota de débito de e-Ticket Venta por Cuenta Ajena";
-	$facturactaaje = 0; //case 141: return "e-Factura Venta por Cuenta Ajena";
-	$ncfacturactaaje = 0; //case 142: return "Nota de crédito de e-Factura Venta por Cuenta Ajena";
-	$ndfaturactaaje = 0; //case 143: return "Nota de débito de e-Factura Venta por Cuenta Ajena";
-	$boletaentrada = 0; //case 151: return "e-Boleta de entrada";
-	$ncboletaentrada = 0; //case 152: return "Nota de crédito de e-Boleta de entrada";
-	$ndboletaentrada = 0; //case 153: return "Nota de débito de e-Boleta de entrada";
-	$remito = 0; //case 181: return "e-Remito";
-	$resguardo = 0; //case 182: return "e-Resguardo";
+	if ( $companie['estado'] == 6 ){
 
-	$vouchersList = file_get_contents(URL_REST.'company/'.$rut.'/cfe/emitidos?From='.$from.'&To='.$to.'&PageSize='.$limit, false, $contexto);
+		echo nl2br("Empresa ".$companie["rut"]."\n");
 
-	$vouchersList = json_decode($vouchersList);
+		$ticket = 0;//case 101: return "e-Ticket";
+		$ncticket = 0; //case 102: return "Nota de crédito de e-Ticket";
+		$ndticket = 0; //case 103: return "Nota de débito de e-Ticket";
+		$ticketconti = 0;//case 201: return "e-Ticket Contingencia";
+		$factura = 0; //case 111: return "e-Factura";
+		$ncfactura = 0; //case 112: return "Nota de crédito de e-Factura";
+		$ndfactura = 0; //case 113: return "Nota de débito de e-Factura";
+		$facturaconti = 0; //case 211: return "e-Factura Contingencia";
+		$facturaexp = 0; //case 121: return "e-Factura Exportación";
+		$ncfacturaexp = 0; //case 122: return "Nota de crédito de e-Factura Exportación";
+		$ndfacturaexp = 0; //case 123: return "Nota de débito de e-Factura Exportación";
+		$remitoexp = 0; //case 124: return "e-Remito de Exportación";
+		$ticketctaaje = 0; //case 131: return "e-Ticket Venta por Cuenta Ajena";
+		$ncticketctaaje = 0; //case 132: return "Nota de crédito de e-Ticket Venta por Cuenta Ajena";
+		$ndticketctaaje = 0; //case 133: return "Nota de débito de e-Ticket Venta por Cuenta Ajena";
+		$facturactaaje = 0; //case 141: return "e-Factura Venta por Cuenta Ajena";
+		$ncfacturactaaje = 0; //case 142: return "Nota de crédito de e-Factura Venta por Cuenta Ajena";
+		$ndfaturactaaje = 0; //case 143: return "Nota de débito de e-Factura Venta por Cuenta Ajena";
+		$boletaentrada = 0; //case 151: return "e-Boleta de entrada";
+		$ncboletaentrada = 0; //case 152: return "Nota de crédito de e-Boleta de entrada";
+		$ndboletaentrada = 0; //case 153: return "Nota de débito de e-Boleta de entrada";
+		$remito = 0; //case 181: return "e-Remito";
+		$resguardo = 0; //case 182: return "e-Resguardo";
+		$otros = 0;
 
-	foreach ($vouchersList as $voucher) {
-		//$voucher = json_decode($voucher);
-		switch ($voucher->tipoCFE) {
-			case 101: $ticket = $ticket+1; break;
-			case 102: $ncticket = $ncticket+1; break;
-			case 103: $ndticket = $ndticket+1; break;
-			case 201: $ticketconti = $ticketconti+1; break;
-			case 111: $factura = $factura+1; break;
-			case 112: $ncfactura = $ncfactura+1; break;
-			case 113: $ndfactura = $ndfactura+1; break;
-			case 211: $facturaconti = $facturaconti+1; break;
-			case 121: $facturaexp = $facturaexp+1; break;
-			case 122: $ncfacturaexp = $ncfacturaexp+1; break;
-			case 123: $ndfacturaexp = $ndfacturaexp+1; break;
-			case 124: $remitoexp = $remitoexp+1; break;
-			case 131: $ticketctaaje = $ticketctaaje+1; break;
-			case 132: $ncticketctaaje = $ncticketctaaje+1; break;
-			case 133: $ndticketctaaje = $ndticketctaaje+1; break;
-			case 141: $facturactaaje = $facturactaaje+1; break;
-			case 142: $ncfacturactaaje = $ncfacturactaaje+1; break;
-			case 143: $ndfaturactaaje = $ndfaturactaaje+1; break;
-			case 151: $boletaentrada = $boletaentrada+1; break;
-			case 152: $ncboletaentrada = $ncboletaentrada+1; break;
-			case 153: $ndboletaentrada = $ndboletaentrada+1; break;
-			case 181: $remito = $remito+1; break;
-			case 182: $resguardo = $resguardo+1; break;
-			default: echo nl2br($voucher->id." no se reconoce tipoCFE ".$voucher->tipoCFE."\n");
-		}
-	}
+		$vouchersList = file_get_contents(URL_REST.'reports/'.$companie["rut"].'?Detallado=1&Desde='.$from.'&Hasta='.$to, false, $contexto);
 
-	$arrayCountVouchers = array(
-		"101" => $ticket,
-		"102" => $ncticket,
-		"103" => $ndticket,
-		"201" => $ticketconti,
-		"111" => $factura,
-		"112" => $ncfactura,
-		"113" => $ndfactura,
-		"211" => $facturaconti,
-		"121" => $facturaexp,
-		"122" => $ncfacturaexp,
-		"123" => $ndfacturaexp,
-		"124" => $remitoexp,
-		"131" => $ticketctaaje,
-		"132" => $ncticketctaaje,
-		"133" => $ndticketctaaje,
-		"141" => $facturactaaje,
-		"142" => $ncfacturactaaje,
-		"143" => $ndfaturactaaje,
-		"151" => $boletaentrada,
-		"152" => $ncboletaentrada,
-		"153" => $ndboletaentrada,
-		"181" => $remito,
-		"182" => $resguardo
-	);
+		$vouchersList = json_decode($vouchersList);
+		foreach ($vouchersList->reportes as $dia) {
+
+			if( count($dia->detalle) > 0 ){
 
 
-	foreach ($arrayCountVouchers as $tipo => $cantidad) {
+				foreach ($dia->detalle as $voucher) {
+					switch ($voucher->tipoCFE) {
+						case 101: $ticket = $ticket+1; break;
+						case 102: $ncticket = $ncticket+1; break;
+						case 103: $ndticket = $ndticket+1; break;
+						case 201: $ticketconti = $ticketconti+1; break;
+						//case 202: $ticketconti = $ticketconti+1; break;
+						case 111: $factura = $factura+1; break;
+						case 112: $ncfactura = $ncfactura+1; break;
+						case 113: $ndfactura = $ndfactura+1; break;
+						case 211: $facturaconti = $facturaconti+1; break;
+						//case 212: $facturaconti = $facturaconti+1; break;
+						case 121: $facturaexp = $facturaexp+1; break;
+						case 122: $ncfacturaexp = $ncfacturaexp+1; break;
+						case 123: $ndfacturaexp = $ndfacturaexp+1; break;
+						case 124: $remitoexp = $remitoexp+1; break;
+						case 131: $ticketctaaje = $ticketctaaje+1; break;
+						case 132: $ncticketctaaje = $ncticketctaaje+1; break;
+						case 133: $ndticketctaaje = $ndticketctaaje+1; break;
+						case 141: $facturactaaje = $facturactaaje+1; break;
+						case 142: $ncfacturactaaje = $ncfacturactaaje+1; break;
+						case 143: $ndfaturactaaje = $ndfaturactaaje+1; break;
+						case 151: $boletaentrada = $boletaentrada+1; break;
+						case 152: $ncboletaentrada = $ncboletaentrada+1; break;
+						case 153: $ndboletaentrada = $ndboletaentrada+1; break;
+						case 181: $remito = $remito+1; break;
+						case 182: $resguardo = $resguardo+1; break;
+						default: $otros = $otros+1; brak;
 
-		if ( $cantidad > 0 ){
-			$sql = "INSERT INTO `uso_cfes` (`rut`, `tipocfe`, `cantidadUsados`, `periodo`) VALUES ($rut, $tipo, $cantidad, '202209')";
 
-			$query = $connection->prepare($sql);
-			$query->execute();
-			$result = $query->get_result();
-			if($result !== false){
-				echo nl2br("Consulta que se intentó hacer ".$sql." resultado obtenido ".$result."\n");
+					}
+				}
+
 			}
 
 		}
+
+		$arrayCountVouchers = array(
+			"101" => $ticket,
+			"102" => $ncticket,
+			"103" => $ndticket,
+			"201" => $ticketconti,
+			"111" => $factura,
+			"112" => $ncfactura,
+			"113" => $ndfactura,
+			"211" => $facturaconti,
+			"121" => $facturaexp,
+			"122" => $ncfacturaexp,
+			"123" => $ndfacturaexp,
+			"124" => $remitoexp,
+			"131" => $ticketctaaje,
+			"132" => $ncticketctaaje,
+			"133" => $ndticketctaaje,
+			"141" => $facturactaaje,
+			"142" => $ncfacturactaaje,
+			"143" => $ndfaturactaaje,
+			"151" => $boletaentrada,
+			"152" => $ncboletaentrada,
+			"153" => $ndboletaentrada,
+			"181" => $remito,
+			"182" => $resguardo,
+			"otros" => $otros
+		);
+
+		$vouchersJson = json_encode($arrayCountVouchers);
+		$sql = "INSERT INTO `uso_cfes` (`rut`, `datos`, `periodo`) VALUES (".$companie['rut'].", '".$vouchersJson."', '202209')";
+		$query = $connection->prepare($sql);
+		$query->execute();
+		$result = $query->get_result();
+		if($result !== false){
+			echo nl2br("Consulta que se intentó hacer ".$sql." resultado obtenido ".$result."\n");
+		}
+		echo nl2br("Procesada\n");
+		echo nl2br("---------------------------------------\n");
+
 	}
-	echo nl2br("Procesada\n");
-	echo nl2br("---------------------------------------\n");
 }
 
 
 echo nl2br("Cerrando conexión\n");
 mysqli_close($connection);
 echo nl2br("Saliendo\n");
+
 exit;
 ?>
