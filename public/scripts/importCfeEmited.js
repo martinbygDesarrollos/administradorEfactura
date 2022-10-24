9272a6fd925c435d8b5cdb78bcf30f50c963185d
Martin B&G Desarrollo de Sistemas <martin@hit.com.uy>
2022-10-24 18:58:00 -0300

    progress bar al iportar xml

--- public/scripts/importCfeEmited.js
+++ public/scripts/importCfeEmited.js
@@ -5,15 +5,19 @@ $("#idFormLoadCfeXml").submit((e)=>{
 	e.preventDefault();
 	$("#idButtonSubmitLoadCfeXml").attr("disabled", true);
 
+	progressBarIdProcess = loadPrograssBar();
+	$("#progressbar").modal("show");
 
 
 	formData = new FormData(document.getElementById("idFormLoadCfeXml"));
 
 	sendAsyncPostForm("importCfeEmitedXml", formData)
 	.then((response)=>{
+		stopPrograssBar(progressBarIdProcess);
+		$('#progressbar').modal("hide");
+
 		$("#idButtonSubmitLoadCfeXml").removeAttr("disabled");
 
-		console.log(response);
 		showMessage(response.result, response.message);
 	})
 
