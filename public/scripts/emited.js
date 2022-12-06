5f37e00888cceaeb42846e45267f15afbb420d9f
Martin B&G Desarrollo de Sistemas <martin@hit.com.uy>
2022-12-06 14:44:34 -0300

    comprobantes para reenviar sobres xml

--- public/scripts/emited.js
+++ public/scripts/emited.js
@@ -40,8 +40,10 @@ $("#idFormEmited").submit((e)=>{
 
 function addRowToInsertCfeData(){
 
+	let options = '<option value="101" >E-Ticket</option><option value="102" >N. crédito E-Ticket</option><option value="103" >N. débito E-Ticket</option><option value="111" selected>E-Factura</option><option value="112" >N. crédito E-Factura</option><option value="113" >N. débito E-Factura</option><option value="121" >E-Factura Exportación</option><option value="122" >N. crédito E-Factura Exportación</option><option value="123" >N. débito E-Factura Exportación</option><option value="124" >E-Remito de Exportación</option><option value="131" >E-Ticket Venta por Cuenta Ajena</option><option value="132" >N. crédito E-Ticket Venta por Cuenta Ajena</option><option value="133" >N. débito E-Ticket Venta por Cuenta Ajena</option><option value="141" >E-Factura Venta por Cuenta Ajena</option><option value="142" >N. crédito E-Factura Venta por Cuenta Ajena</option><option value="143" >N. débito E-Factura Venta por Cuenta Ajena</option><option value="151" >E-Boleta de entrada</option><option value="152" >N. crédito E-Boleta de entrada</option><option value="153" >N. débito E-Boleta de entrada</option><option value="181" >E-Remito</option><option value="182" >E-Resguardo</option>';
+
 	let rowNumber = $("#rowsToInsertCfeData").children().length;
-	let newRow = '<div class="form-row" ><div class="form__group field w-25 m-2"><select class="form__field" id="" name="nameSelectTipoCfeSendXml'+rowNumber+'"><option value="111" selected>E-Factura</option><option value="101" >E-Ticket</option></select></div>';
+	let newRow = '<div class="form-row" ><div class="form__group field w-25 m-2"><select class="form__field" id="" name="nameSelectTipoCfeSendXml'+rowNumber+'">'+options+'</select></div>';
 
 	newRow += '<div class="form__group field m-2"><input type="text" id="" name="nameInputSerieCfeSendXml'+rowNumber+'" class="form__field input_uppercase" placeholder="Serie" required/></div>'
 
