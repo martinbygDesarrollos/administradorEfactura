7baf886ebb9f570d2db348178588afd27f1cc3ab
Martin B&G Desarrollo de Sistemas <martin@hit.com.uy>
2023-08-18 12:29:55 -0300

    editar colores comprobante

--- public/scripts/repImpresa.js
+++ public/scripts/repImpresa.js
@@ -8,12 +8,9 @@ function getColorsInfo(){
 		$('#progressbar').modal("hide");
 		if(response.result === 2){
 			console.log(response)
-			console.log();
-
-
-
+			loadDataStyles(response.objectResult);
 		}else if(response.result === 1){
-			showReplyMessage(1, "No se encontraron datos.", "ok");
+			showMessage(response.result, "No se encontraron datos.")
 		}
 		else{
 			window.location.href = getSiteURL() + "cerrar-session";
@@ -23,12 +20,20 @@ function getColorsInfo(){
 }
 
 
-colorColumnEven
-colorColumnOdd
-detailLineColor
-detailLineStyle
-detailLineWidth
-emitterNameSize
-hideDueDate
-hideExchangeRate
-language
+function loadDataStyles(obj){
+
+
+
+
+
+}
+
+/*
+"colorColumnOdd": "#FFFFFF", //columnas descripcion y cantidad
+    "colorColumnEven": null, //columnas precio unitario
+    "detailLineWidth": null, //ancho de la linea que separa las filas de detalle ej "1px"
+    "detailLineColor": null, //color de la linea que separa las filas de detalle
+    "detailLineStyle": "dashed", //diseño de la linea que separa las filas de detalle ej solid, dashed
+    "textColorColumnPrimary": null, //color del texto de la columna total y #
+    "emitterNameSize": "1.1em", //tamaño del nombre de quien emite
+    */
